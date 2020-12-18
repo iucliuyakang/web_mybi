435bb49c7f7ee330e0c0f01935b95817e23b82a7
lk <ipangyo826@163.com>
2020-12-18 17:25:01 +0800

    完成个人中心的功能开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -34,17 +34,7 @@ function getUserInfo() {
             renderAvatar(res.data); //调用函数渲染用户的头像
 
         },
-        // 不论成功还是失败都会调用complete这个函数
-        complete: function(res) {
 
-            // 在 complete 回调函数中，可以使用res.responseJSON 拿到服务器相应回来的数据
-            if (res.responseJSON.status === 1 && res.responseJSON.massage === '身份认证失败！') {
-                // 1.强制清空 token
-                localStorage.removeItem('token')
-                    // 2.强制跳转到登录页面
-                location.href = '/login.html'
-            }
-        }
     })
 }
 // 渲染用户的头像
@@ -59,12 +49,12 @@ function renderAvatar(user) {
         $('.layui-nav-img')
             .attr('src', user.user_pic)
             .show()
-        $('.text-avatar').hide()
+        $('.text-avater').hide()
     } else {
         // 3.2 渲染文本头像
         $('.layui-nav-img').hide()
         var first = name[0].toUpperCase()
-        $('.text-avatar')
+        $('.text-avater')
             .html(first)
             .show()
     }
